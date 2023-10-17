b8378c76d2fcdd360ae39aa50a72181cfbc7d6ca
Karken Turarov <karken_turarov@mail.ru>
2023-10-17 06:53:08 +0600

    add eslint src files jest abel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,65 +1,71 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
+  env: {
+    "browser": true,
+    "es2021": true,
+    "jest/globals": true,
+    "node": true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:jest/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  overrides: [
+    {
+      files: ["test/**", ".eslintrc.{js,cjs}"],
+      plugins: ["jest", "css", "html"],
+      extends: ["plugin:jest/recommended"],
+      rules: { "jest/prefer-expect-assertions": "off" },
+      env: {
         "jest/globals": true,
         "node": true,
+      },
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "extends": [
-        'eslint:recommended',
-        "plugin:jest/recommended",
-        "prettier",
-        'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    root: true,
-    "overrides": [
-        {
-            files: ["test/**", ".eslintrc.{js,cjs}"],
-            plugins: ["jest"],
-            extends: ["plugin:jest/recommended"],
-            rules: { "jest/prefer-expect-assertions": "off" },
-            "env": {
-                "jest/globals": true,
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "import/extensions": [
+      "warn",
+      "never",
+      "error",
+      "ignorePackages",
+      {
+        js: "always",
+      },
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    plugins: ["jest", "css", "html"],
-    "rules": {
-        "import/extensions": [
-            "error",
-            "ignorePackages",
-            {
-                js: "always",
-            },
-        ],
-        "semi": ["error", "always"],
-        "quotes": ["error", "double"],
-        "jest/no-disabled-tests": "warn",
-        "jest/no-focused-tests": "error",
-        "jest/no-identical-title": "error",
-        "jest/prefer-to-have-length": "warn",
-        "jest/valid-expect": "error",
-        "max-len": [
-            "error",
-            { ignoreTrailingComments: true },
-            {
-                ignoreComments: true,
-            },
-            {
-                code: 100,
-            },
-        ],
-    }
-}
+    "import/prefer-default-export": "off",
+    "jest/valid-title": "off",
+    "no-console": "off",
+    "no-alert": "off",
+    "no-restricted-globals": "off",
+    "no-plusplus": "off",
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error",
+    "max-len": [
+      "error",
+      { ignoreTrailingComments: true },
+      {
+        ignoreComments: true,
+      },
+      {
+        code: 100,
+      },
+    ],
+    "import/no-unresolved": "off",
+  },
+};
