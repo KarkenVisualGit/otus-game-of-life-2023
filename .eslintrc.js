3868d97a986fb0ba9b031b8123340907f6e50c15
Karken Turarov <karken_turarov@mail.ru>
2023-10-19 22:17:24 +0600

    modified eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,10 +14,6 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   root: true,
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
   rules: {
     "import/extensions": ["warn", "never"],
     "import/prefer-default-export": "off",
